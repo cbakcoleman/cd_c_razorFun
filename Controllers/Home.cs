ad58706c8dec2d4302cfbc4ad7578b772285ad97
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-30 17:26:33 -0700

    attempt to fix route

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace cd_c_razorFun.Controllers
 {
     public class IndexController : Controller
     {
+        
+
         [Route("")]
         [HttpGet]
-        public ViewResult Index()
+        public ViewResult Index(List<string> food)
         {
+            ViewBag.food = food;
             return View();
         }
     }
